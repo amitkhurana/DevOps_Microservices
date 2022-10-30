16fa7a9cddf7da0243e4576841a3467b3cdd53e9
Amit Khurana <amit.khurana@gmail.com>
2022-10-30 18:50:00 +0530

    Task 4

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,6 +6,7 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=amitkhurana/udacity-ml-msapi
 
 # Step 2:  
 # Authenticate & tag
@@ -13,3 +14,6 @@ echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker login -u amitkhurana
+docker tag udacity-ml-msapi $dockerpath:latest
+docker push $dockerpath:latest
